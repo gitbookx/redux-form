27744398f8d4ba41a1a0af979362d574b7c12a8f
Andrew Luca <thendrluca@gmail.com>
2020-04-10 00:47:20 +0300

    fix: improve `SubmissionError` detection
    Now event after obfuscation, or importing SubmissionForm
    from a different context, `isSubmissionError` will return true
    only for `SubmissionError`
    
    Closes #4641

---
+++ src/__tests__/SubmissionError.spec.js
@@ -0,0 +1,14 @@
+// @flow
+
+import ExtendableError from 'es6-error'
+import { isSubmissionError, SubmissionError } from '../SubmissionError'
+
+describe('isSubmissionError', () => {
+  it('should return `true` only when argument is instance `SubmissionError`', () => {
+    expect(isSubmissionError(new SubmissionError({}))).toBe(true)
+    expect(isSubmissionError(new Error())).toBe(false)
+    expect(isSubmissionError(new ExtendableError())).toBe(false)
+    expect(isSubmissionError({})).toBe(false)
+    expect(isSubmissionError()).toBe(false)
+  })
+})
